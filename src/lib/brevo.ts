e80e0d0aadbffa7ab379d59d57056a9057e2ef4b
Juanco <juanffo874@gmail.com>
2024-08-25 22:43:20 -0600

    Configuracion del envio de correo

--- src/lib/brevo.ts
+++ src/lib/brevo.ts
@@ -8,3 +8,16 @@ apiInstance.setApiKey(
     process.env.BREVO_API_KEY as string 
 )
 
+//Cofigurando el SMTP
+const smtpEmail = new brevo.SendSmtpEmail()
+
+export async function sendEmail() {
+    smtpEmail.subject = "Hello World"
+    smtpEmail.to = [
+        { email: "juanffo874@gmail.com", name: "Juanco" }
+    ]
+    smtpEmail.htmlContent = '<html><body><h1>Hola mundo</h1></body></html>'
+    smtpEmail.sender = { name: "Juan Cruz", email: "210373@utxicotepec.edu.mx" }
+
+    await apiInstance.sendTransacEmail(smtpEmail)
+}
